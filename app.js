1d6affc7b7db37e57d81c3357788c4845c5866e6
Arjun Patel <patel.arjun50@gmail.com>
2018-07-30 21:38:47 -0700

    newest changes

--- app.js
+++ app.js
@@ -1,25 +1,65 @@
 var express = require("express");
 var app = express();
-var port = 3000;
+var path = require('path');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3000;
+
+// to read request's body object/dict
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://heroku_rg3dpg4f:3trhj8bvk2g9lp3ocvtbltgvp9@ds259711.mlab.com:59711/heroku_rg3dpg4f";
 
 app.use(express.static('assets'));
 console.log("Finished Building static files for web page!");
 
-
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://heroku_rg3dpg4f:3trhj8bvk2g9lp3ocvtbltgvp9@ds259711.mlab.com:59711/heroku_rg3dpg4f";
-var db;
-
-MongoClient.connect(url, function (err, client) {
-  if (err) return console.log(err);
-  console.log('Connected!');
-  db = client.db('heroku_rg3dpg4f')
-  app.listen(port, () => {
-   console.log("Server listening on port " + port);
-  });
-  db.close();
+app.use("/home", (req, res) => {
+ res.sendFile(__dirname + "/upload.html");
 });
 
-app.use("/", (req, res) => {
- res.sendFile(__dirname + "/assets/upload.html");
+
+app.listen(PORT, () => {
+ console.log("Server listening on port " + PORT);
+});
+
+
+app.post('/insertorders', function (req, res) {
+  var orders = req.body.orders;
+
+  MongoClient.connect(url, function (err, client) {
+    if (err) return console.log(err);
+    console.log('Connected to Database! Inserting orders');
+
+    var db = client.db("heroku_rg3dpg4f");
+    db.collection('orders').insertMany(orders, function (err, result) {
+      if (err) return console.log(err);
+      return console.log("INSERTED ALL ORDERS");
+    });
+
+    client.close();
+  });
+
+  res.end('INSERTED ALL ORDERS');
+});
+
+app.get('/getorders', function (req, res) {
+  MongoClient.connect(url, function (err, client) {
+    if (err) return console.log(err);
+    console.log('Connected to Database! Getting orders!');
+
+    var db = client.db("heroku_rg3dpg4f");
+    db.collection('orders').find().toArray(function (err, items) {
+      if (err) return console.log(err);
+
+      console.log("INSERTED ALL ORDERS");
+      return res.status(200).send({
+        error: false,
+        message: "successfully retrieved",
+        result: items
+      });
+    });
+
+    client.close();
+  });
 });
--- assets/main.js
+++ assets/main.js
@@ -26,10 +26,47 @@ $(function() {
   // General function to parse JSON
     function onReaderLoad(event) {
         var obj = JSON.parse(event.target.result);
-        console.log(obj.orders);
-        obj.orders.map((order) => {
-          console.log(order);
-          $('#table').append("<div class='flexBasicRow'><p class='col'>" + order.customer_id + "</p><p class='col'>" + order.order_no + "</p><p class='col'>" + order.part_id + "</p><p class='col'>" + order.parent_part + "</p><p class='col'>" + order.price + "</p><p class='col'>" + order.qty + "</p></p><p class='col'>" + order.paid + "</p></div>").fadeIn();
-        });
+        console.log(obj);
+
+        insertOrders(obj);
+        getOrders(addToDOM);
+    }
+
+    addToDOM = (orders) => {
+      orders.map((order) => {
+        $('#table').append("<div class='flexBasicRow'><p class='col'>" + order.customer_id + "</p><p class='col'>" + order.order_no + "</p><p class='col'>" + order.part_id + "</p><p class='col'>" + order.parent_part + "</p><p class='col'>" + order.price + "</p><p class='col'>" + order.qty + "</p></p><p class='col'>" + order.paid + "</p></div>").fadeIn();
+      });
+    }
+
+    insertOrders = (orders) => {
+      $.ajax({
+        type: "POST",
+        url: "http://localhost:3000/insertorders",
+        data: orders,
+        success: function (data) {
+          alert("Success");
+        },
+        dataType: "json",
+        error : function(request, error) {
+            console.log("Request: " + JSON.stringify(request));
+        }
+      });
+    }
+
+    getOrders = (callback) => {
+      $.ajax({
+        type: "GET",
+        url: "http://localhost:3000/getorders",
+        data: {},
+        success: function (res) {
+          orders = res.result;
+
+          callback(orders);
+        },
+        dataType: "json",
+        error : function(request, error) {
+            console.log("Request: " + JSON.stringify(request));
+        }
+      });
     }
 });
